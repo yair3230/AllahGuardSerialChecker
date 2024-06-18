a5a6be0bec37f22b258d5f21b2f7980e953aed0d
YairCyeSec <yair.ulmayev@cyesec.com>
2024-06-18 15:53:50 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,107 @@
+import tkinter as tk
+from tkinter import messagebox
+import winreg
+
+def gfew(vvc):
+    mmop = False
+    if not vvc.isalpha():
+        mmop = True
+    if vvc[0] != vvc[3]:
+        mmop = True
+    if ord(vvc[0]) != ord(vvc[-1]) - 4:
+        mmop = True
+    if not vvc[1].islower():
+        mmop = True
+    if not vvc[2].isalnum():
+        mmop = True
+    if mmop:
+        return 'Key check failed on the first segment'
+    return None
+
+
+def gfaw(vvc):
+    kklop = vvc[6:11]
+    mmop = False
+    if not kklop.isnumeric():
+        mmop = True
+    if int(kklop) % 2 != 0:
+        mmop = True
+    if kklop[0] == '0':
+        mmop = True
+    if kklop[0] != kklop[-1]:
+        mmop = True
+    if ord(vvc[0]) != int(kklop[:2]):
+        mmop = True
+    if mmop:
+        return 'Key check failed on the second segment'
+    return None
+
+
+def gfaf(vvc):
+    kklop = vvc[12:]
+    mmop = False
+    if ord(kklop[0]) >> 2 != 20:
+        mmop = True
+    diff = ord(vvc[4]) & ord(vvc[5])
+    if diff > 10:
+        diff = diff % 10
+    if kklop[1] != str(diff):
+        mmop = True
+    if not kklop[2:].isnumeric():
+        mmop = True
+    num = 0
+    try:
+        num = int(kklop[2:])
+    except ValueError:
+        mmop = True
+    if ord(vvc[0]) << 1 != num:
+        mmop = True
+    if mmop:
+        return 'Key check failed on the third segment'
+    return None
+
+
+def ppap(vvc):
+
+    if len(vvc) != 17:
+        return 'Invalid key format'
+
+    if vvc[5] != '-' or vvc[11] != '-':
+        return 'Invalid key format'
+
+    error = gfew(vvc[:5])
+    if error:
+        return error
+    error = gfaw(vvc)
+    if error:
+        return error
+    error = gfaf(vvc)
+    if error:
+        return error
+    return None
+
+
+def main():
+    error = None
+    value = None
+    messagebox.showinfo('Info', 'AllahGuardSerialChecker will now check for a serial key.'
+                                'Please donate to our open source project!')
+    try:
+        reg_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Software\\MicroAndSoft')
+        value, _ = winreg.QueryValueEx(reg_key, 'serial_key')
+    except FileNotFoundError:
+        error = 'Serial key not found'
+    if not error:
+        error = ppap(value)
+    if error:
+        # Hide the root TK window
+        root = tk.Tk()
+        root.withdraw()
+
+        # Show a message box with the error using tkinter
+        messagebox.showerror('Error', error)
+        exit(-1)
+
+
+if __name__ == '__main__':
+    main()
